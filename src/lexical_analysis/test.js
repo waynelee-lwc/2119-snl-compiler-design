867c94ed823ea225e74a3de39200172d885798ca
liweicong.lwc <liweicong.lwc@bytedance.com>
2022-03-16 17:20:33 +0800

    [feat]增加token 错误 日志 状态节点对象

--- src/lexical_analysis/test.js
+++ src/lexical_analysis/test.js
@@ -1,7 +1,21 @@
 
 var dict = require('./dict')
 var checking = require('./checking')
+var rw = require('./rw')
+var model = require('./model')
 
-console.log(dict.LegalCharacters['a'])
-console.log(checking.IsLegal('a'))
-console.log(checking.IsLowerLetter)
+// console.log(dict.LegalCharacter['a'])
+// console.log(checking.IsLegal('a'))
+// console.log(checking.IsLowerLetter)
+
+// console.log(rw.LoadSourceCode('readingtest.txt').split('\n'))
+// console.log(__dirname)
+
+// var token = new model.Token('testtype','testname',13)
+
+// console.log(JSON.stringify(token))
+
+let code = rw.LoadSourceCode('readingtest.txt').split('\n')
+for(idx in code){
+    console.log(Number(idx) + 1,code[idx])
+}
