ef3280cf2264add08f6f19030df1de3aa9745a03
liweicong.lwc <liweicong.lwc@bytedance.com>
2022-03-16 11:32:27 +0800

    [feat]起草了词法分析的字典和校验

---
+++ src/lexical_analysis/checking.js
@@ -0,0 +1,73 @@
+
+/* 
+    校验检测部分代码
+*/
+
+
+/* 单字符检测 */
+//合法字符
+exports.IsLegal = (ch)=>{
+
+}
+//数字
+exports.IsNumber = (ch)=>{
+    
+}
+//非0数字
+exports.IsNonZeroNumber = (ch)=>{
+
+}
+//字母
+exports.IsLetter = (ch)=>{
+
+}
+//小写字母
+exports.IsLowerLetter = (ch)=>{
+
+}
+//大写字母
+exports.IsUpperLetter = (ch)=>{
+
+}
+//分界符
+exports.IsSeperator = (ch)=>{
+
+}
+//字符起始结束符
+exports.IsCharacterMarker = (ch)=>{
+
+}
+
+/* token类型检测 */
+//合法ID
+exports.IsLegalID = (token)=>{
+
+}
+//无符号整数
+exports.IsUnsignedInteger = (token)=>{
+
+}
+//保留字
+exports.IsReserved = (token)=>{
+
+}
+//单字符分界符
+exports.IsSingleSeperator = (token)=>{
+
+}
+//双字符分界符号
+exports.IsDoubleSeperator = (token)=>{
+
+}
+//注释头符
+exports.IsCommentHeader = (token)=>{
+
+}
+//注释尾符
+exports.IsCommentEnder = (token)=>{
+
+}
+//数组下标界限符
+exports.IsArrayIndexRange = (token)=>{
+
+}
