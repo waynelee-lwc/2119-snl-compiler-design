9ea47c942953fbb639e6b755a917f606c12aed46
liweicong.lwc <liweicong.lwc@bytedance.com>
2022-04-04 14:29:36 +0800

    [feat]调整输出目录格式，增加web服务端

---
+++ src/coordinator_webserver/server.js
@@ -0,0 +1,93 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let fs = require('fs')
+let pathTool = require('path')
+let {exec, execSync} = require('child_process')
+
+const { dir } = require('console')
+
+let app = express()
+app.use(bodyParser.json())
+const NONE_SENSE_LEN = 10
+const PORT = 3008
+
+function formatNumber(num){
+    return num < 10 ? '0'+num : ''+num
+}
+
+function currDateTime(){
+    let curr = new Date()
+
+    let year    = formatNumber(curr.getFullYear())
+    let month   = formatNumber(curr.getMonth())
+    let day     = formatNumber(curr.getDay())
+    let hour    = formatNumber(curr.getHours())
+    let minutes = formatNumber(curr.getMinutes())
+    let seconds = formatNumber(curr.getSeconds())
+    return `${year}-${month}-${day}_${hour}:${minutes}:${seconds}`
+}
+
+function geneRandomStr(len){
+    str = ''
+    for(let i = 0;i < len;i++){
+        str += String.fromCharCode(Math.floor(Math.random()*26)+65)
+    }
+    return str
+}
+
+function geneProgramName(){
+    return `${currDateTime()}_${geneRandomStr(NONE_SENSE_LEN)}`
+}
+
+function Save(dir,filename,content){
+    let path = pathTool.resolve(dir,filename)
+
+    fs.writeFileSync(path,content)
+}
+
+function Load(dir,filename){
+    let path = pathTool.resolve(dir,filename)
+    return fs.readFileSync(path).toString()
+}
+
+
+/* webserver */
+
+app.post('/compile',(req,res)=>{
+    console.log(JSON.stringify(req.body))
+    let src = req.body.src
+    let programName = geneProgramName()
+    let programPath = pathTool.resolve(__dirname,`../../outputs/cache/${programName}`)
+    fs.mkdirSync(programPath)
+
+    Save(programPath,`snl`,src) //保存程序文件
+    let lexbuf = execSync(`node ../lexical_analysis/main.js ${programPath}`) //词法分析
+    let synbuf = execSync(`../syntax_analysis/recursive_descent/runnable ${programPath}`) //语法分析
+
+    // console.log(lexbuf.toString())
+    // console.log(synbuf.toString())
+    
+    let result = {}
+    let files = fs.readdirSync(programPath)
+    for(let file of files){
+        str = Load(programPath,file)
+        result[file] = str
+    }
+
+    let resp = {
+        snl : result['snl'],
+        tree : result['tree'],
+        tokens : result['tk'],
+        gene : result['gene'],
+        lex_err:result['lexerr'],
+        syn_err:result['syn_err'],
+        program_name : programName
+    }
+    fs.rmdirSync(programPath,{ recursive: true, force: true })
+    res.send(resp)
+})
+
+let server = app.listen(PORT,()=>{
+    console.log('The server is listening on port :',PORT)
+})
+
